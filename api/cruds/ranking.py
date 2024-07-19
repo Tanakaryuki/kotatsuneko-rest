6e2504a9ff535decc7c5443f0555b5f0ebaa77e7
Tanakaryuki <tnk3257.rt@gmail.com>
2024-07-19 23:05:59 +0900

    fix: Remove "limit" field from ranking creation

--- api/cruds/ranking.py
+++ api/cruds/ranking.py
@@ -6,6 +6,7 @@ import api.schemas.ranking as ranking_schema
 
 def create_ranking(db: client, ranking: ranking_schema.RankingRequest, username: str) -> tuple:
     ranking_dict = ranking.model_dump()
+    ranking_dict.pop("limit")
     ranking_dict["update_at"] = datetime.now()
     user_ref = db.collection("Ranking").document(username)
     user_ref.set(ranking_dict)
