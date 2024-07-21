d20d313b1ef55314eb8efffd0b7191cc8ca4a34c
Tanakaryuki <tnk3257.rt@gmail.com>
2024-07-22 04:12:35 +0900

    fix: Improve error handling for invalid username patterns

--- api/routers/user.py
+++ api/routers/user.py
@@ -14,7 +14,7 @@ router = APIRouter()
 @router.post("/signup", description="新しいアカウントを作成するために使用されます。")
 def signup(request: user_schema.UserSignupRequest, db:client = Depends(get_db)):
     if contains_invalid_pattern(request.username):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={ "type": "invalid_pattern", "loc": [ "body", "username" ], "msg": "Invalid username pattern. The username should not start or end with a period, contain consecutive periods, double underscores, or slashes", "input": request.username })
     user = user_crud.read_user_by_username(db=db, username=request.username)
     if user:
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
@@ -25,7 +25,7 @@ def signup(request: user_schema.UserSignupRequest, db:client = Depends(get_db)):
 @router.get("/users/username/exists", description="指定されたユーザー名が存在するかどうかを確認します。",response_model=user_schema.UserExistsResponse)
 def check_username(username: str = Query(..., min_length=1, max_length=50), db:client = Depends(get_db)):
     if contains_invalid_pattern(username):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={ "type": "invalid_pattern", "loc": [ "username" ], "msg": "Invalid username pattern. The username should not start or end with a period, contain consecutive periods, double underscores, or slashes", "input": username })
     user = user_crud.read_user_by_username(db=db, username=username)
     if user:
         return user_schema.UserExistsResponse(exists=True)
@@ -35,7 +35,7 @@ def check_username(username: str = Query(..., min_length=1, max_length=50), db:c
 @router.post("/token", description="アクセストークンを取得するために使用されます。",response_model=user_schema.Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db:client = Depends(get_db)):
     if contains_invalid_pattern(form_data.username):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={ "type": "invalid_pattern", "loc": [ "body", "username" ], "msg": "Invalid username pattern. The username should not start or end with a period, contain consecutive periods, double underscores, or slashes", "input": form_data.username })
     user = authenticate_user(db=db, username=form_data.username, password=form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
